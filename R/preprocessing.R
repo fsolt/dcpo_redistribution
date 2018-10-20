0b3bf66e940fe1c8e60be637f8f15df074e91fd0
Frederick Solt <frederick-solt@uiowa.edu>
2018-10-20 10:53:55 -0500

    save outfile

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,8 +1,5 @@
 library(tidyverse)
 library(DCPO)
 
-# red <- dcpo_setup(vars = "data-raw/surveys_redist.csv",
-# 									file = "data/all_data_redist.csv")
-
-red <- dcpo_setup(vars = read_csv("data-raw/surveys_redist.csv", col_types = "ccccc"))
-
+red <- dcpo_setup(vars = "data-raw/surveys_redist.csv",
+                  file = "data/all_data_redist.csv")
